169e9ca9ad5903b61f768b544b6ce57e8e0c82be
levenkov <artem@amperka.ru>
2015-03-20 19:27:18 +0300

    Bug fix.

--- lib/posix-fileutils/fileutils.rb
+++ lib/posix-fileutils/fileutils.rb
@@ -27,203 +27,201 @@ module FileUtils
     :dsync => [:v, ].to_set,
   }
 
-  class << self
-    def parse_cp_args src, dst, *opts
-      raise ArgumentError unless !dst.a?
+  def parse_cp_args src, dst, *opts
+    raise ArgumentError unless !dst.a?
 
-      dst = Pathname.new dst.to_s unless dst.kind_of? Pathname
+    dst = Pathname.new dst.to_s unless dst.kind_of? Pathname
 
-      if src.a?
-        return true if src.count == 0
+    if src.a?
+      return true if src.count == 0
 
-        raise ArgumentError unless dst.directory?
+      raise ArgumentError unless dst.directory?
 
-        src.map! do |elem| Pathname.new elem.to_s unless elem.kind_of? Pathname end
+      src.map! do |elem| Pathname.new elem.to_s unless elem.kind_of? Pathname end
 
-        class << src
-          def to_s
-            inject('') do |accu, elem| "#{accu}#{elem.to_s} " end.chomp ' '
-          end
-        end
-      else
-        src = Pathname.new src.to_s unless src.kind_of? Pathname
-      end
-
-      class << opts
-        attr_accessor :src
-        attr_accessor :dst
-
+      class << src
         def to_s
-          opts_s = '-f '
-
-          [:T, :a, :v, :r].each do |opt|
-            opts_s << "-#{opt.to_s} " if include?(opt) || $fudeopt.include?(opt)
-          end
-
-          opts_s
+          inject('') do |accu, elem| "#{accu}#{elem.to_s} " end.chomp ' '
         end
       end
-
-      opts.src, opts.dst = src, dst
-
-      [src, dst, opts]
+    else
+      src = Pathname.new src.to_s unless src.kind_of? Pathname
     end
-    module_function :parse_cp_args
 
-    def cp src, dst, *opts
-      opts = opts.to_set
+    class << opts
+      attr_accessor :src
+      attr_accessor :dst
 
-      src, dst, opts = parse_cp_args src, dst, *opts
+      def to_s
+        opts_s = '-f '
 
-      opts << :r if src.a? ? src.inject(false) do |a,p| p.directory? | a end : src.directory?
-
-      Kernel.system "cp #{opts.to_s}#{src.to_s} #{dst.to_s}"
-    end
-    module_function :cp
-
-    def dsync src, dst, *opts
-      src, dst, opts = parse_cp_args src, dst, *opts
-
-      opts = opts.to_set
-      
-      raise ArgumentError unless src.directory? && dst.directory?
-
-      Dir["#{dst.to_s}/**/{*,.*}"].each do |file|
-        file = Pathname.new file
-        next unless file.exist?
-
-        srcfile = src + file.relative_path_from(dst)
-
-        (rm file, *(opts&@popts[:rm]) or return false) unless srcfile.exist?
-      end
-      
-      Dir["#{src.to_s}/**/{*,.*}"].each do |file|
-        file = Pathname.new file
-        next if file.directory?
-
-        dstfile = dst + file.relative_path_from(src)
-
-        (mkdir dstfile.dirname, *((opts+[:p])&@popts[:mkdir]) or return false) unless dstfile.dirname.directory?
-
-        next if dstfile.file? && Digest::SHA256.file(file) == Digest::SHA256.file(dstfile)
-
-        cp file, dstfile, *(opts&@popts[:cp]) or return false
-      end
-
-      true
-    end
-    module_function :dsync
-
-    def mv src, dst, *opts
-      opts = opts.to_set
-
-      src, dst, opts = parse_cp_args src, dst, *opts
-
-      Kernel.system "mv #{opts.to_s}#{src.to_s} #{dst.to_s}"
-    end
-    module_function :mv
-
-    def parse_list_args list, *opts
-      if list.a?
-        return true if list.count == 0
-
-        list.map! do |elem| Pathname.new elem.to_s unless elem.kind_of? Pathname end
-
-        class << list
-          def to_s
-            inject('') do |accu,elem| "#{accu}#{elem.to_s} " end.chomp ' '
-          end
+        [:T, :a, :v, :r].each do |opt|
+          opts_s << "-#{opt.to_s} " if include?(opt) || $fudeopt.include?(opt)
         end
-      else
-        list = Pathname.new list.to_s unless list.kind_of? Pathname
+
+        opts_s
       end
+    end
 
-      class << opts
-        attr_accessor :list
-        attr_accessor :flags
+    opts.src, opts.dst = src, dst
 
+    [src, dst, opts]
+  end
+  module_function :parse_cp_args
+
+  def cp src, dst, *opts
+    opts = opts.to_set
+
+    src, dst, opts = parse_cp_args src, dst, *opts
+
+    opts << :r if src.a? ? src.inject(false) do |a,p| p.directory? | a end : src.directory?
+
+    Kernel.system "cp #{opts.to_s}#{src.to_s} #{dst.to_s}"
+  end
+  module_function :cp
+
+  def dsync src, dst, *opts
+    src, dst, opts = parse_cp_args src, dst, *opts
+
+    opts = opts.to_set
+    
+    raise ArgumentError unless src.directory? && dst.directory?
+
+    Dir["#{dst.to_s}/**/{*,.*}"].each do |file|
+      file = Pathname.new file
+      next unless file.exist?
+
+      srcfile = src + file.relative_path_from(dst)
+
+      (rm file, *(opts&@popts[:rm]) or return false) unless srcfile.exist?
+    end
+    
+    Dir["#{src.to_s}/**/{*,.*}"].each do |file|
+      file = Pathname.new file
+      next if file.directory?
+
+      dstfile = dst + file.relative_path_from(src)
+
+      (mkdir dstfile.dirname, *((opts+[:p])&@popts[:mkdir]) or return false) unless dstfile.dirname.directory?
+
+      next if dstfile.file? && Digest::SHA256.file(file) == Digest::SHA256.file(dstfile)
+
+      cp file, dstfile, *(opts&@popts[:cp]) or return false
+    end
+
+    true
+  end
+  module_function :dsync
+
+  def mv src, dst, *opts
+    opts = opts.to_set
+
+    src, dst, opts = parse_cp_args src, dst, *opts
+
+    Kernel.system "mv #{opts.to_s}#{src.to_s} #{dst.to_s}"
+  end
+  module_function :mv
+
+  def parse_list_args list, *opts
+    if list.a?
+      return true if list.count == 0
+
+      list.map! do |elem| Pathname.new elem.to_s unless elem.kind_of? Pathname end
+
+      class << list
         def to_s
-          opts_s = ''
-
-          @flags.each do |opt|
-            opts_s << "-#{opt.to_s} " if include?(opt) || $fudeopt.include?(opt)
-          end
-
-          opts_s
+          inject('') do |accu,elem| "#{accu}#{elem.to_s} " end.chomp ' '
         end
       end
-
-      opts.list = list
-
-      [list, opts]
+    else
+      list = Pathname.new list.to_s unless list.kind_of? Pathname
     end
-    module_function :parse_list_args
 
-    def rm list, *opts
-      opts = opts.to_set
+    class << opts
+      attr_accessor :list
+      attr_accessor :flags
 
-      list, opts = parse_list_args list, *opts
-      opts.flags = [:v, :f, :r]
+      def to_s
+        opts_s = ''
 
-      opts << :r if list.a? ? list.inject(false) do |a,p| p.directory? | a end : list.directory?
-
-      Kernel.system "rm #{opts.to_s}#{list.to_s}"
-    end
-    module_function :rm
-
-    def mkdir list, *opts
-      opts = opts.to_set
-
-      list, opts = parse_list_args list, *opts
-      opts.flags = [:v, :p]
-
-      if opts.include? :f
-        unless list.a?
-          return true if list.directory?
-        else
-          list.select! do |elem| !elem.directory? end
-          return true if list.empty?
+        @flags.each do |opt|
+          opts_s << "-#{opt.to_s} " if include?(opt) || $fudeopt.include?(opt)
         end
+
+        opts_s
       end
-
-      Kernel.system "mkdir #{opts.to_s}#{list.to_s}"
     end
-    module_function :mkdir
 
-    def touch list, *opts
-      opts = opts.to_set
+    opts.list = list
 
-      list, opts = parse_list_args list, *opts
-      opts.flags = [:v]
+    [list, opts]
+  end
+  module_function :parse_list_args
 
-      Kernel.system "touch #{opts.to_s}#{list.to_s}"
+  def rm list, *opts
+    opts = opts.to_set
+
+    list, opts = parse_list_args list, *opts
+    opts.flags = [:v, :f, :r]
+
+    opts << :r if list.a? ? list.inject(false) do |a,p| p.directory? | a end : list.directory?
+
+    Kernel.system "rm #{opts.to_s}#{list.to_s}"
+  end
+  module_function :rm
+
+  def mkdir list, *opts
+    opts = opts.to_set
+
+    list, opts = parse_list_args list, *opts
+    opts.flags = [:v, :p]
+
+    if opts.include? :f
+      unless list.a?
+        return true if list.directory?
+      else
+        list.select! do |elem| !elem.directory? end
+        return true if list.empty?
+      end
     end
-    module_function :touch
 
-    alias_method :_cd, :cd
+    Kernel.system "mkdir #{opts.to_s}#{list.to_s}"
+  end
+  module_function :mkdir
 
-    def cd list, *opts
-      opts = opts.to_set
+  def touch list, *opts
+    opts = opts.to_set
 
-      list, opts = parse_list_args list, *opts
-      opts.flags = [:v]
+    list, opts = parse_list_args list, *opts
+    opts.flags = [:v]
 
-      raise ArgumentError if list.a?
+    Kernel.system "touch #{opts.to_s}#{list.to_s}"
+  end
+  module_function :touch
 
-      return false unless list.directory?
+  alias_method :_cd, :cd
 
-      _cd list.to_s, opts.include?(:v) ? {:verbose => true} : {:verbose => false}
+  def cd list, *opts
+    opts = opts.to_set
 
-      true
-    end
-    module_function :cd
+    list, opts = parse_list_args list, *opts
+    opts.flags = [:v]
 
-    def pwd
-      Pathname.new(`pwd`.chomp)
-    end
-    module_function :pwd
+    raise ArgumentError if list.a?
 
-  end # class << self
+    return false unless list.directory?
+
+    _cd list.to_s, opts.include?(:v) ? {:verbose => true} : {:verbose => false}
+
+    true
+  end
+  module_function :cd
+
+  def pwd
+    Pathname.new(`pwd`.chomp)
+  end
+  module_function :pwd
+
 end # module FileUtils
 
 # vim: sw=2 sts=2 ts=8:
